f81bb4c2666fb0ea5aed3b64606c1b258bdb5e7a
Rahul Ambhore <rahulambhore394@gmail.com>
2024-12-22 17:01:41 +0530

    First Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,4 +61,6 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:okhttp:4.10.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+
 }
