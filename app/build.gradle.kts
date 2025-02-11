5c44d7eb1d70c9739b5712373e66a2993a6748b2
rahulambhore394 <rahulambhore394@gmail.com>
2025-02-11 14:25:40 +0530

    build: setup app module build script, compileOptions, and viewBinding configuration

---
+++ app/build.gradle.kts
@@ -0,0 +1,77 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.google.gms.google.services)
+}
+
+android {
+    namespace = "com.rahul.safebite"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.rahul.safebite"
+        minSdk = 27
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.firebase.database)
+    implementation(libs.play.services.location)
+    implementation(libs.play.services.maps)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation ("com.intuit.ssp:ssp-android:1.0.5")
+    implementation ("com.intuit.sdp:sdp-android:1.0.5")
+    implementation ("com.airbnb.android:lottie:3.4.0")
+    implementation ("androidx.fragment:fragment-ktx:1.3.6")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation ("androidx.camera:camera-core:1.3.0")
+    implementation ("androidx.camera:camera-camera2:1.3.0")
+    implementation ("androidx.camera:camera-lifecycle:1.3.0")
+    implementation ("androidx.camera:camera-view:1.3.0")
+    implementation ("org.tensorflow:tensorflow-lite:2.8.0") // TensorFlow Lite
+    implementation ("org.tensorflow:tensorflow-lite-support:0.3.1") // Support library
+    implementation ("com.google.mlkit:image-labeling:17.0.7")
+
+    implementation ("org.jsoup:jsoup:1.16.1")
+
+
+
+}
